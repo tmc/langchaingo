0a6dfe3dff7ef657367d5bdc186f934d53945505
Travis Cline <travis.cline@gmail.com>
2023-04-18 19:38:15 -0700

    llms/local: Add comments

--- llms/local/localllm.go
+++ llms/local/localllm.go
@@ -4,23 +4,35 @@ import (
 	"context"
 	"errors"
 	"os"
+	"os/exec"
 
 	"github.com/tmc/langchaingo/llms"
 	"github.com/tmc/langchaingo/llms/local/internal/localclient"
 )
 
-const localLLMBinVarName = "LOCAL_LLM_BIN"
-const localLLMArgsVarName = "LOCAL_LLM_ARGS"
+const (
+	// The name of the environment variable that contains the path to the local LLM binary.
+	localLLMBinVarName = "LOCAL_LLM_BIN"
+	// The name of the environment variable that contains the CLI arguments to pass to the local LLM binary.
+	localLLMArgsVarName = "LOCAL_LLM_ARGS"
+)
 
-var ErrEmptyResponse = errors.New("no response")
-var ErrMissingBin = errors.New("missing the local LLM binary path, set the LOCAL_LLM_BIN environment variable")
+var (
+	// ErrEmptyResponse is returned when the local LLM binary returns an empty response.
+	ErrEmptyResponse = errors.New("no response")
+	// ErrMissingBin is returned when the LOCAL_LLM_BIN environment variable is not set.
+	ErrMissingBin = errors.New("missing the local LLM binary path, set the LOCAL_LLM_BIN environment variable")
+)
 
+// LLM is a local LLM implementation.
 type LLM struct {
 	client *localclient.Client
 }
 
+// _ ensures that LLM implements the llms.LLM interface.
 var _ llms.LLM = (*LLM)(nil)
 
+// Call calls the local LLM binary with the given prompt.
 func (o *LLM) Call(prompt string) (string, error) {
 	r, err := o.Generate([]string{prompt})
 	if err != nil {
@@ -32,6 +44,7 @@ func (o *LLM) Call(prompt string) (string, error) {
 	return r[0].Text, nil
 }
 
+// Generate generates completions using the local LLM binary.
 func (o *LLM) Generate(prompts []string) ([]*llms.Generation, error) {
 	result, err := o.client.CreateCompletion(context.TODO(), &localclient.CompletionRequest{
 		Prompt: prompts[0],
@@ -44,17 +57,18 @@ func (o *LLM) Generate(prompts []string) ([]*llms.Generation, error) {
 	}, nil
 }
 
+// New creates a new local LLM implementation.
 func New() (*LLM, error) {
 	// Require the user to set the path to the local LLM binary
-	binPath := os.Getenv(localLLMBinVarName)
-	if binPath == "" {
-		return nil, ErrMissingBin
+	bin := os.Getenv(localLLMBinVarName)
+	path, err := exec.LookPath(bin)
+	if err != nil {
+		return nil, errors.Join(ErrMissingBin, err)
 	}
 
 	// Allow the user to pass CLI arguments to the local LLM binary (optional)
 	args := os.Getenv(localLLMArgsVarName)
-
-	c, err := localclient.New(binPath, args)
+	c, err := localclient.New(path, args)
 	return &LLM{
 		client: c,
 	}, err
