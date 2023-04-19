db5b2e412144ffc082d9357bb4bdad33eb3f02bf
Travis Cline <travis.cline@gmail.com>
2023-04-18 19:53:55 -0700

    schema: Add schema package

---
+++ schema/schema.go
@@ -0,0 +1,29 @@
+package schema
+
+// AgentAction is the agent's action to take.
+type AgentAction struct {
+	Tool      string
+	ToolInput any
+	Log       string
+}
+
+// AgentFinish is the agent's return value.
+type AgentFinish struct {
+	ReturnValues map[string]any
+	Log          string
+}
+
+// Generation is the output of a single generation.
+type Generation struct {
+	// Generated text output.
+	Text string
+	// Raw generation info response from the provider.
+	// May include things like reason for finishing (e.g. in OpenAI).
+	GenerationInfo map[string]any
+}
+
+// LLMResult is the class that contains all relevant information for an LLM Result.
+type LLMResult struct {
+	Generations [][]Generation
+	LLMOutput   map[string]any
+}
