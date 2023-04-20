3b0019725bbd7d9d5f10bd87b48364bdd693740f
Luis.Fernando <luisfmiranda8@gmail.com>
2023-04-20 17:21:52 -0500

    fedining the agent type and interfaces

--- exp/agents/agents.go
+++ exp/agents/agents.go
@@ -1,10 +1,51 @@
 // Package agents defines the types for langchaingo Agetns.
 package agents
 
-type Agent interface {
-	Run(query string) (string, error)
+import (
+	"errors"
+
+	"github.com/tmc/langchaingo/exp/agents/agentExecutor"
+	"github.com/tmc/langchaingo/exp/agents/agentExecutor/mrkl"
+	"github.com/tmc/langchaingo/exp/tools"
+	"github.com/tmc/langchaingo/llms"
+)
+
+type AgentOptions map[string]any
+type AgentType string
+
+const (
+	OneShotZeroAgent AgentType = "OneShotZeroAgent"
+)
+
+type Options func(p *AgentOptions)
+
+func defaultOptions() AgentOptions {
+	return AgentOptions{
+		"verbose": false,
+	}
 }
 
-func Run(agent Agent, query string) (string, error) {
-	return agent.Run(query)
+func WithVerbosity() Options {
+	return func(p *AgentOptions) {
+		(*p)["verbose"] = true
+	}
+}
+
+func InitializeAgent(
+	llm llms.LLM,
+	tools []tools.Tool,
+	agentType AgentType,
+	opts ...Options,
+
+) (agentExecutor.AgentExecutor, error) {
+	options := defaultOptions()
+	for _, opt := range opts {
+		opt(&options)
+	}
+	switch agentType {
+	case OneShotZeroAgent:
+		return mrkl.NewOneShotAgent(llm, tools, options)
+	default:
+		return nil, errors.New("Unknown agent type")
+	}
 }
