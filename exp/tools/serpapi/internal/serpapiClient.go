be6fd673ef1bd36f6358fffeb54753741f7fb942
Luis.Fernando <luisfmiranda8@gmail.com>
2023-04-20 02:06:03 -0500

    fixing panic: assignment to entry in nil map

--- exp/tools/serpapi/internal/serpapiClient.go
+++ exp/tools/serpapi/internal/serpapiClient.go
@@ -34,9 +34,9 @@ func New() (*SerpapiClient, error) {
 }
 
 func (s *SerpapiClient) Search(query string) (string, error) {
-	var params url.Values
+	params := make(url.Values)
 	query = strings.ReplaceAll(query, " ", "+")
-	params.Set("q", query)
+	params.Add("q", query)
 	params.Add("google_domain", "google.com")
 	params.Add("gl", "us")
 	params.Add("hl", "en")
