47d8ef95a52b1134e87ba4a190b6dd9f032f47e8
Luis.Fernando <luisfmiranda8@gmail.com>
2023-04-20 22:03:31 -0500

    creatin a Initialize agent

--- exp/agents/agents.go
+++ exp/agents/agents.go
@@ -1,7 +1,14 @@
 // Package agents defines the types for langchaingo Agetns.
 package agents
 
-import ()
+import (
+	"errors"
+
+	agentsExecutor "github.com/tmc/langchaingo/exp/agents/agentExecutor"
+	"github.com/tmc/langchaingo/exp/agents/mrkl"
+	"github.com/tmc/langchaingo/exp/tools"
+	"github.com/tmc/langchaingo/llms"
+)
 
 type AgentType string
 
@@ -21,5 +28,24 @@ func defaultOptions() AgentOptions {
 func WithVerbosity() Options {
 	return func(p *AgentOptions) {
 		(*p)["verbose"] = true
+	}
+}
+
+func InitializeAgent(
+	llm llms.LLM,
+	tools []tools.Tool,
+	agentType AgentType,
+	opts ...Options,
+
+) (agentsExecutor.AgentExecutor, error) {
+	options := defaultOptions()
+	for _, opt := range opts {
+		opt(&options)
+	}
+	switch agentType {
+	case ZeroShotReactDescription:
+		return mrkl.NewOneShotAgent(llm, tools, options)
+	default:
+		return nil, errors.New("Unknown agent type")
 	}
 }
