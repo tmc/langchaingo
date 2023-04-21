3ac900125e10da3e4a8f404cb00ffc7f5b3c4bcd
Luis.Fernando <luisfmiranda8@gmail.com>
2023-04-21 08:43:33 -0500

    adding doc.go

---
+++ exp/agent/mrkl/doc.go
@@ -0,0 +1,11 @@
+// Package mrkl provides an implementation of the OneShotZeroAgent,
+// which is responsible for executing queries and returning results using
+// the LLM model, tools, and an internal chain. The package also includes
+// related structures and methods to support the agent's functionality.
+//
+// The OneShotZeroAgent struct implements the AgentExecutor interface
+// and provides methods to run queries and manage the agent's internal state.
+//
+// The package also includes utility functions for checking options,
+// planning, and running tools to perform various operations during query execution.
+package mrkl
