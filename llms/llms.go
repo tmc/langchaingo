6d18be57542e28caf453fd1858fc064879a78058
Travis Cline <travis.cline@gmail.com>
2023-05-02 09:20:55 -0700

    llms: Refactor options interface

--- llms/llms.go
+++ llms/llms.go
@@ -5,8 +5,8 @@ import "context"
 
 // LLM is a langchaingo Large Language Model.
 type LLM interface {
-	Call(ctx context.Context, prompt string, stopWords []string) (string, error)
-	Generate(ctx context.Context, prompts []string, stopWords []string) ([]*Generation, error)
+	Call(ctx context.Context, prompt string, options ...CallOption) (string, error)
+	Generate(ctx context.Context, prompts []string, options ...CallOption) ([]*Generation, error)
 }
 
 // Generation is a single generation from a langchaingo LLM.
