b7c4e7d3c2b7097acfb400d43604c64461c7461a
Owen Wu <yubing744@163.com>
2023-05-03 17:39:51 +0800

    feat: new openai llm support option

---
+++ llms/openai/openaillm_option.go
@@ -0,0 +1,43 @@
+package openai
+
+import (
+	"os"
+	"sync"
+)
+
+const (
+	tokenEnvVarName = "OPENAI_API_KEY" //nolint:gosec
+	modelEnvVarName = "OPENAI_MODEL"   //nolint:gosec
+)
+
+var (
+	initOptions    sync.Once
+	defaultOptions *options
+)
+
+type options struct {
+	token string
+	model string
+}
+
+type Option func(*options)
+
+// initOpts initializes defaultOptions with the environment variables.
+func initOpts() {
+	defaultOptions = &options{
+		token: os.Getenv(tokenEnvVarName),
+		model: os.Getenv(modelEnvVarName),
+	}
+}
+
+func WithToken(token string) Option {
+	return func(opts *options) {
+		opts.token = token
+	}
+}
+
+func WithModel(model string) Option {
+	return func(opts *options) {
+		opts.model = model
+	}
+}
