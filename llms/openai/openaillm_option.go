0e37d479bd3677d7555df9823db2ab9400a40959
Alexander Knipping <alexander.knipping@exaring.de>
2023-05-23 13:41:23 +0200

    Refactor(llms): simplify option handling
    The sync.once is not needed, the default options can just be initialized
    in when New() is called.

--- llms/openai/openaillm_option.go
+++ llms/openai/openaillm_option.go
@@ -1,21 +1,8 @@
 package openai
 
-import (
-	"os"
-	"sync"
-)
-
 const (
 	tokenEnvVarName = "OPENAI_API_KEY" //nolint:gosec
 	modelEnvVarName = "OPENAI_MODEL"   //nolint:gosec
-)
-
-var (
-	// nolint: gochecknoglobals
-	initOptions sync.Once
-
-	// nolint: gochecknoglobals
-	defaultOptions *options
 )
 
 type options struct {
@@ -24,14 +11,6 @@ type options struct {
 }
 
 type Option func(*options)
-
-// initOpts initializes defaultOptions with the environment variables.
-func initOpts() {
-	defaultOptions = &options{
-		token: os.Getenv(tokenEnvVarName),
-		model: os.Getenv(modelEnvVarName),
-	}
-}
 
 // WithToken passes the OpenAI API token to the client. If not set, the token
 // is read from the OPENAI_API_KEY environment variable.
