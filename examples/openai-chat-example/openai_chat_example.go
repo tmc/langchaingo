54c6782c60803ab68a33a1cb86c1f93973b5fb69
Travis Cline <travis.cline@gmail.com>
2023-05-26 17:51:07 -0700

    examples: Update examples

--- examples/openai-chat-example/openai_chat_example.go
+++ examples/openai-chat-example/openai_chat_example.go
@@ -6,6 +6,7 @@ import (
 	"log"
 
 	"github.com/tmc/langchaingo/llms/openai"
+	"github.com/tmc/langchaingo/schema"
 )
 
 func main() {
@@ -14,10 +15,12 @@ func main() {
 		log.Fatal(err)
 	}
 	ctx := context.Background()
-	completion, err := llm.Chat(ctx, []openai.ChatMessage{{Role: "user", Content: "What would be a good company name a company that makes colorful socks?"}})
+	completion, err := llm.Chat(ctx, []schema.ChatMessage{
+		schema.HumanChatMessage{Text: "What would be a good company name a company that makes colorful socks?"},
+	})
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println(completion.Content)
+	fmt.Println(completion.Message.Text)
 }
