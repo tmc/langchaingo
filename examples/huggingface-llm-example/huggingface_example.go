b7342fbc995f4039aeba65de3de3bc9c8798fcd7
Paul Rock <paul@drteam.rocks>
2023-05-29 01:54:10 +0300

    Options passing with replaced RepoId added to example

--- examples/huggingface-llm-example/huggingface_example.go
+++ examples/huggingface-llm-example/huggingface_example.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/tmc/langchaingo/llms"
 	"github.com/tmc/langchaingo/llms/huggingface"
 )
 
@@ -14,7 +15,11 @@ func main() {
 		log.Fatal(err)
 	}
 	ctx := context.Background()
-	completion, err := llm.Call(ctx, "What would be a good company name be for name a company that makes colorful socks?")
+
+	var generateOptions []llms.CallOption
+	generateOptions = append(generateOptions, llms.WithRepoId("gpt2"))
+
+	completion, err := llm.Call(ctx, "What would be a good company name be for name a company that makes colorful socks?", generateOptions...)
 	if err != nil {
 		log.Fatal(err)
 	}
