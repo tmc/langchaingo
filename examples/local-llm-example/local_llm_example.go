e9d3d5c4b5e75d43cdd2fd84489ebe5113a1b313
Paul Rock <paul@drteam.rocks>
2023-06-17 17:39:35 +0300

    New fetures added to local-llm example

--- examples/local-llm-example/local_llm_example.go
+++ examples/local-llm-example/local_llm_example.go
@@ -9,12 +9,36 @@ import (
 )
 
 func main() {
+
+	// You may instantiate a client with a default bin and args from environment variable
 	llm, err := local.New()
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	// Or instantiate a client with a custom bin and args options
+	//clientOptions := []local.Option{
+	//	local.WithBin("/usr/bin/echo"),
+	//	local.WithArgs("--arg1=value1 --arg2=value2"),
+	//	local.WithGlobalAsArgs(), // build key-value arguments from global llms.Options, then append to args
+	//}
+	//llm, err := local.New(clientOptions...)
+
+	// Init context
 	ctx := context.Background()
+
+	// By default, library will use default bin and args
 	completion, err := llm.Call(ctx, "How many sides does a square have?")
+
+	// Or append to default args options from global llms.Options
+	//generateOptions := []llms.CallOption{
+	//	llms.WithTopK(10),
+	//	llms.WithTopP(0.95),
+	//	llms.WithSeed(13),
+	//}
+	// In that case command will look like: /path/to/bin --arg1=value1 --arg2=value2 --top_k=10 --top_p=0.95 --seed=13 "How many sides does a square have?"
+	//completion, err := llm.Call(ctx, "How many sides does a square have?", generateOptions...)
+
 	if err != nil {
 		log.Fatal(err)
 	}
