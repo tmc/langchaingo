4d7a8e246edc4090e66b1b437d360ea67a99d3f7
Taiwo Hassan Yusuf <teehazzan@gmail.com>
2023-06-19 09:35:48 +0100

    Added package level comments to codebase

--- embeddings/doc.go
+++ embeddings/doc.go
@@ -1,3 +1,13 @@
-// Package embeddings contains a standard interface and implementations
-// for creating numerical representation of textual data
+/*
+Package embeddings contains the implementation for creating vector embeddings from text using different APIs, like OpenAI and Google PaLM (VertexAI).
+
+The main components of this package are:
+
+- Embedder interface: a common interface for creating vector embeddings from texts.
+- OpenAI: an Embedder implementation using the OpenAI API.
+- VertexAIPaLM: an Embedder implementation using Google PaLM (VertexAI) API.
+- Helper functions: utility functions for embedding, such as `batchTexts` and `maybeRemoveNewLines`.
+
+The package provides a flexible way to handle different APIs for generating embeddings by using the Embedder interface as an abstraction.
+*/
 package embeddings
--- textsplitter/doc.go
+++ textsplitter/doc.go
@@ -1,3 +1,12 @@
-// Package textsplitter contains a standard interface and implementations for splitting
-// text and creating documents.
+/*
+Package textsplitter provides tools for splitting long texts into smaller chunks based on configurable rules and parameters. It aims to help in processing these chunks more efficiently when interacting with language models or other text-processing tools.
+
+The main components of this package are:
+
+- TextSplitter interface: a common interface for splitting texts into smaller chunks.
+- RecursiveCharacter: a text splitter that recursively splits texts by different characters (separators) combined with chunk size and overlap settings.
+- Helper functions: utility functions for creating documents out of split texts and rejoining them if necessary.
+
+Using the TextSplitter interface, developers can implement custom splitting strategies for their specific use cases and requirements.
+*/
 package textsplitter
