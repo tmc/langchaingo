44eb35c38aeeec9b99725a3b4e73a252421958f2
FluffyKebab <aksel@aksels.net>
2023-06-27 16:16:56 +0200

    agents: add memory to executor chain and add test to conversational

---
+++ agents/conversational_test.go
@@ -0,0 +1,38 @@
+package agents
+
+import (
+	"context"
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"github.com/tmc/langchaingo/chains"
+	"github.com/tmc/langchaingo/llms/openai"
+	"github.com/tmc/langchaingo/memory"
+	"github.com/tmc/langchaingo/tools"
+)
+
+func TestConversationalWithMemory(t *testing.T) {
+	t.Parallel()
+	if openaiKey := os.Getenv("OPENAI_API_KEY"); openaiKey == "" {
+		t.Skip("OPENAI_API_KEY not set")
+	}
+
+	llm, err := openai.New()
+	require.NoError(t, err)
+
+	executor, err := Initialize(
+		llm,
+		[]tools.Tool{tools.Calculator{}},
+		ConversationalReactDescription,
+		WithMemory(memory.NewBuffer()),
+	)
+
+	res, err := chains.Run(context.Background(), executor, "Hi! my name is Bob and the year I was born is 1987")
+	require.NoError(t, err)
+
+	res, err = chains.Run(context.Background(), executor, "What is the year I was born times 34")
+	require.NoError(t, err)
+	require.True(t, strings.Contains(res, "67558"), `result does not contain the correct answer '67558'`)
+}
