bed5886b5834df15ef3c5cd3f04ace8689be2cd5
FluffyKebab <aksel@aksels.net>
2023-07-08 00:01:01 +0200

    docs: update pairty matrix

--- chains/question_answering.go
+++ chains/question_answering.go
@@ -30,6 +30,7 @@ Return any relevant text verbatim.
 Question: {{.question}}
 Relevant text, if any:`
 
+//nolint:lll
 const _defaultMapReduceCombineQATemplate = `Given the following extracted parts of a long document and a question, create a final answer. 
 If you don't know the answer, just say that you don't know. Don't try to make up an answer.
 
