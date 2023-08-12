ad1b8d4826a3f995e2f6e5282e2724f95fb2e42e
Simun Strukan <simun.strukan@gmail.com>
2023-08-12 21:56:27 +0200

    Added links and documents tools interface impl

--- tools/metaphor/documents.go
+++ tools/metaphor/documents.go
@@ -1,1 +1,76 @@
 package metaphor
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"gpt/util/tools/metaphor/client"
+	"os"
+	"strings"
+
+	"github.com/tmc/langchaingo/tools"
+)
+
+type MetaphorContents struct {
+	client *client.MetaphorClient
+}
+
+var _ tools.Tool = &MetaphorContents{}
+
+func NewDocuments(options ...client.ClientOptions) (*MetaphorContents, error) {
+	apiKey := os.Getenv("METAPHOR_API_KEY")
+	if apiKey == "" {
+		return nil, ErrMissingToken
+	}
+
+	client, err := client.NewClient(apiKey, options...)
+	if err != nil {
+		return nil, err
+	}
+
+	return &MetaphorContents{
+		client: client,
+	}, nil
+
+}
+
+func (tool *MetaphorContents) Name() string {
+	return "Metaphor Contents Extractor"
+}
+
+func (tool *MetaphorContents) Description() string {
+	return `
+	To be used with Metaphor Search and/or Metaphor Links Search Tool.
+	Retrieve contents of web pages based on a list of ID strings.
+	Input should be a list(or a single ID) of comma seperated IDs,  
+	obtained from either Metaphor Search or Metaphor Search Links tool.
+	Expected input format:
+	"8U71IlQ5DUTdsherhhYA,9segZCZGNjjQB2yD2uyK,..."`
+}
+
+func (tool *MetaphorContents) Call(ctx context.Context, input string) (string, error) {
+	ids := strings.Split(input, ",")
+	for i, id := range ids {
+		ids[i] = strings.TrimSpace(id)
+	}
+
+	contents, err := tool.client.GetContents(ctx, ids)
+	if err != nil {
+		if errors.Is(err, client.ErrNoContentExtracted) {
+			return "Metaphor Extractor didn't return any results", nil
+		}
+		return "", err
+	}
+
+	return tool.formatContents(contents), nil
+}
+
+func (tool *MetaphorContents) formatContents(response *client.ContentsResponse) string {
+	formattedResults := ""
+
+	for _, result := range response.Contents {
+		formattedResults += fmt.Sprintf("Title: %s\nContent: %s\nURL: %s\n\n", result.Title, result.Extract, result.Url)
+	}
+
+	return formattedResults
+}
--- tools/metaphor/links.go
+++ tools/metaphor/links.go
@@ -1,1 +1,66 @@
 package metaphor
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"gpt/util/tools/metaphor/client"
+	"os"
+
+	"github.com/tmc/langchaingo/tools"
+)
+
+type MetaphorLinksSearch struct {
+	client *client.MetaphorClient
+}
+
+var _ tools.Tool = &MetaphorLinksSearch{}
+
+func NewLinksSearch(options ...client.ClientOptions) (*MetaphorLinksSearch, error) {
+	apiKey := os.Getenv("METAPHOR_API_KEY")
+	if apiKey == "" {
+		return nil, ErrMissingToken
+	}
+
+	client, err := client.NewClient(apiKey, options...)
+	if err != nil {
+		return nil, err
+	}
+	metaphor := &MetaphorLinksSearch{
+		client: client,
+	}
+
+	return metaphor, nil
+}
+
+func (tool *MetaphorLinksSearch) Name() string {
+	return "Metaphor Links Search"
+}
+
+func (tool *MetaphorLinksSearch) Description() string {
+	return `
+	Metaphor Links Search finds similar links to the link provided.
+	Input should be the url string for which you would like to find similar links`
+}
+
+func (tool *MetaphorLinksSearch) Call(ctx context.Context, input string) (string, error) {
+	links, err := tool.client.FindSimilar(ctx, input)
+	if err != nil {
+		if errors.Is(err, client.ErrNoLinksFound) {
+			return "Metaphor Links Search didn't return any results", nil
+		}
+		return "", err
+	}
+
+	return tool.formatLinks(links), nil
+}
+
+func (tool *MetaphorLinksSearch) formatLinks(response *client.SearchResponse) string {
+	formattedResults := ""
+
+	for _, result := range response.Results {
+		formattedResults += fmt.Sprintf("Title: %s\nURL: %s\nID: %s\n\n", result.Title, result.Url, result.Id)
+	}
+
+	return formattedResults
+}
