cf2ca07b52a6efac80f7c09fbb6dc3f2e227d9e7
Travis Cline <travis.cline@gmail.com>
2023-10-28 15:26:38 -0700

    ollama: Small lint fix

--- llms/ollama/chatoptions.go
+++ llms/ollama/chatoptions.go
@@ -8,8 +8,9 @@ type chatOptions struct {
 type ChatOption func(*chatOptions)
 
 const (
-	// a default chat template for LLama2 from https://www.philschmid.de/llama-2#how-to-prompt-llama-2-chat.
-	// LLamacpp will add the BOS by default https://github.com/ggerganov/llama.cpp/tree/master/examples/server#api-endpoints.
+	// A default chat template for LLama2 from https://www.philschmid.de/llama-2#how-to-prompt-llama-2-chat.
+	// LLamacpp will add the BOS by default,
+	// See https://github.com/ggerganov/llama.cpp/tree/master/examples/server#api-endpoints for more details.
 	_defaultLLamaChatTemplate = `[INST] <<SYS>>
 {{.system}}
 <</SYS>>
