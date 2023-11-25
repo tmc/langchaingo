7889a0f0a8998b4d5edce4094e4396794c72b9de
Eli Bendersky <eliben@users.noreply.github.com>
2023-11-25 11:42:32 -0800

    examples/ernie-completion-examples: make this example a separate module part 2 (#367)
    This sets the go.mod to point to a newer version of the parent module,
    so now there's no conflict and everything builds and lints.
    
    This is part 2, followup on #365

--- examples/ernie-completion-example/ernie_completion_example.go
+++ examples/ernie-completion-example/ernie_completion_example.go
@@ -1,35 +1,45 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"log"
+
+	ernieembedding "github.com/tmc/langchaingo/embeddings/ernie"
+	"github.com/tmc/langchaingo/llms"
+	"github.com/tmc/langchaingo/llms/ernie"
+)
+
 func main() {
-	//llm, err := ernie.New(ernie.WithModelName(ernie.ModelNameERNIEBot))
+	llm, err := ernie.New(ernie.WithModelName(ernie.ModelNameERNIEBot))
 	// note:
 	// You would include ernie.WithAKSK(apiKey,secretKey) to use specific auth info.
 	// You would include ernie.WithModelName(ernie.ModelNameERNIEBot) to use the ERNIE-Bot model.
-	//if err != nil {
-	//log.Fatal(err)
-	//}
-	//ctx := context.Background()
-	//completion, err := llm.Call(ctx, "介绍一下你自己",
-	//llms.WithTemperature(0.8),
-	//llms.WithStreamingFunc(func(ctx context.Context, chunk []byte) error {
-	//log.Println(string(chunk))
-	//return nil
-	//}),
-	//)
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx := context.Background()
+	completion, err := llm.Call(ctx, "介绍一下你自己",
+		llms.WithTemperature(0.8),
+		llms.WithStreamingFunc(func(ctx context.Context, chunk []byte) error {
+			log.Println(string(chunk))
+			return nil
+		}),
+	)
 
-	//if err != nil {
-	//log.Fatal(err)
-	//}
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	//_ = completion
+	_ = completion
 
-	//// embedding
-	//embedding, _ := ernieembedding.NewErnie()
+	// embedding
+	embedding, _ := ernieembedding.NewErnie()
 
-	//emb, err := embedding.EmbedDocuments(ctx, []string{"你好"})
+	emb, err := embedding.EmbedDocuments(ctx, []string{"你好"})
 
-	//if err != nil {
-	//log.Fatal(err)
-	//}
-	//fmt.Println("Embedding-V1:", len(emb), len(emb[0]))
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Embedding-V1:", len(emb), len(emb[0]))
 }
