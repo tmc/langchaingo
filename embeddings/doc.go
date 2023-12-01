9508a34a2940f60a0969ca5c669502fd2f308424
Eli Bendersky <eliben@users.noreply.github.com>
2023-12-01 13:05:51 -0800

    embeddings: add example of proper usage, and tweak doc.go accordingly (#393)
    * embeddings: add example of proper usage, and tweak doc.go accordingly
    
    For #379
    
    * Appease lint and add comment

--- embeddings/doc.go
+++ embeddings/doc.go
@@ -1,15 +1,14 @@
 /*
-Package embeddings contains the implementation for creating vector
-embeddings from text using different APIs, like OpenAI and Google PaLM (VertexAI).
+Package embeddings contains helpers for creating vector embeddings from text
+using different providers.
 
 The main components of this package are:
 
-- Embedder interface: a common interface for creating vector embeddings from texts.
-- OpenAI: an Embedder implementation using the OpenAI API.
-- VertexAIPaLM: an Embedder implementation using Google PaLM (VertexAI) API.
-- Helper functions: utility functions for embedding, such as `batchTexts` and `maybeRemoveNewLines`.
+  - [Embedder] interface: a common interface for creating vector embeddings
+    from texts, with optional batching.
+  - [NewEmbedder] creates implementations of [Embedder] from provider LLM
+    (or Chat) clients.
 
-The package provides a flexible way to handle different APIs for generating
-embeddings by using the Embedder interface as an abstraction.
+See the package example below.
 */
 package embeddings
