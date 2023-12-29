04813072309a8e2b401302684d42816922dd6cda
Thomas <thomasmartin.poli@fox.com>
2023-12-29 22:23:32 +0100

    add comment and remove unnecessary type

--- vectorstores/azureaisearch/types.go
+++ vectorstores/azureaisearch/types.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 )
 
+// FieldType type for pseudo enum.
 type FieldType = string
 
+// Pseudo enum for all the different FieldType.
 const (
 	FieldTypeString         FieldType = "Edm.String"
 	FieldTypeSingle         FieldType = "Edm.Single"
@@ -17,10 +19,7 @@ const (
 	FieldTypeComplexType    FieldType = "Edm.ComplexType"
 )
 
+// CollectionField allows to define a fieldtype as a collection.
 func CollectionField(fieldType FieldType) FieldType {
 	return fmt.Sprintf("Collection(%s)", fieldType)
-}
-
-type SimilaritySearchFilters struct {
-	K int
 }
