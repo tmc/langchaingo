91213cbc6fc525c36f1f660e994d8b44b7151d8c
Eli Bendersky <eliben@gmail.com>
2024-01-23 07:30:09 -0800

    move 2

---
+++ llms/googleai/new.go
@@ -0,0 +1,39 @@
+package googleai
+
+import (
+	"context"
+
+	"github.com/google/generative-ai-go/genai"
+	"github.com/tmc/langchaingo/callbacks"
+	"github.com/tmc/langchaingo/llms"
+	"google.golang.org/api/option"
+)
+
+// GoogleAI is a type that represents a Google AI API client.
+type GoogleAI struct {
+	CallbacksHandler callbacks.Handler
+	client           *genai.Client
+	opts             options
+}
+
+var _ llms.Model = &GoogleAI{}
+
+// NewGoogleAI creates a new GoogleAI struct.
+func NewGoogleAI(ctx context.Context, opts ...Option) (*GoogleAI, error) {
+	clientOptions := defaultOptions()
+	for _, opt := range opts {
+		opt(&clientOptions)
+	}
+
+	gi := &GoogleAI{
+		opts: clientOptions,
+	}
+
+	client, err := genai.NewClient(ctx, option.WithAPIKey(clientOptions.apiKey))
+	if err != nil {
+		return gi, err
+	}
+
+	gi.client = client
+	return gi, nil
+}
