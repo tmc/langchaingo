adab41f736f86445548c3b32bc93682506e168a7
Daniel Bos <daniel.bos@sap.com>
2024-01-26 19:26:39 +0800

    llms: add caching functionality for Models
    New caching functionality has been added to langchaingo to boost
    execution speed for repetitive tasks.  Specifically, we implemented
    an in-memory caching for Language Learning Models (LLMs) so that once
    a model generates a content from a sequence of messages, this result
    is stored in the cache. Any subsequent calls with the same sequence of
    messages will read the result from the cache, instead of having the
    LLM regenerate it, this optimization is intended to reduce the
    response time for recurring requests.
    
    This caching functionality is generic, different cache backends can be
    used when creating the wrapper. To accomplish this, the 'llms/cache'
    package was created with a generic wrapper that adds caching to a
    'llms.Model'. The 'llms/cache/inmemory' package was also created for
    the in-memory implementation of the cache.
    
    Additionally, a caching example was included to demonstrate the usage
    of the implemented caching mechanism.
    
    Minor fix: Typo error in 'TotalTokesn' was corrected to 'TotalToken'
    in 'ollama/ollamallm.go'.
    
    Resolves #395

---
+++ examples/caching-llm-example/caching_llm_example.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"strings"
+	"time"
+
+	"github.com/mitchellh/go-wordwrap"
+	"github.com/tmc/langchaingo/llms"
+	"github.com/tmc/langchaingo/llms/cache"
+	"github.com/tmc/langchaingo/llms/cache/inmemory"
+	"github.com/tmc/langchaingo/llms/ollama"
+)
+
+const WIDTH = 80
+
+func main() {
+	ctx := context.Background()
+
+	var llm llms.Model
+
+	// base LLM is ollama/llama2.
+	llm, err := ollama.New(ollama.WithModel("llama2"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// create a new inmemory cache backend that caches results for one minute.
+	mem, err := inmemory.New(ctx, inmemory.WithExpiration(time.Minute))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// wrap the base LLM to make it caching.
+	llm = cache.New(llm, mem)
+
+	// repeat the same query a few times. The first time it'll query the base LLM but
+	// subsequent times the result is returned from the cache.
+	for i := 0; i < 3; i++ {
+		start := time.Now()
+
+		completion, err := llms.GenerateFromSinglePrompt(ctx, llm,
+			"Human: Who was the first man to walk on the moon?\nAssistant:",
+			llms.WithTemperature(0.8),
+		)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if i > 0 {
+			fmt.Println(strings.Repeat("=", WIDTH))
+		}
+
+		completion = wordwrap.WrapString(completion, WIDTH)
+
+		fmt.Printf("## Iteration #%d\n\n%s\n\n(took %v)\n",
+			i, completion, time.Since(start))
+	}
+}
---
+++ llms/cache/inmemory/inmemory_test.go
@@ -0,0 +1,48 @@
+package inmemory
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	cache "github.com/Code-Hex/go-generics-cache"
+	"github.com/Code-Hex/go-generics-cache/policy/fifo"
+	"github.com/stretchr/testify/require"
+	"github.com/tmc/langchaingo/llms"
+)
+
+func TestInMemory(t *testing.T) {
+	t.Parallel()
+
+	ctx := context.Background()
+	rq := require.New(t)
+	ttl := time.Second / 2
+
+	cache, err := New(ctx,
+		WithCacheOptions(
+			cache.AsFIFO[string, *llms.ContentResponse](
+				fifo.WithCapacity(1),
+			),
+		),
+		WithExpiration(ttl),
+	)
+	rq.NoError(err)
+
+	rq.Nil(cache.Get(ctx, "key1"), "empty cache should be empty")
+
+	val := &llms.ContentResponse{
+		Choices: []*llms.ContentChoice{{
+			Content: "value",
+		}},
+	}
+
+	cache.Put(ctx, "key1", val)
+	rq.Equal(val, cache.Get(ctx, "key1"))
+
+	cache.Put(ctx, "key2", val)
+	rq.Nil(cache.Get(ctx, "key1"), "first value should have been evicted")
+	rq.NotNil(cache.Get(ctx, "key2"))
+
+	time.Sleep(ttl * 2) // double the ttl to make sure the value has timed out.
+	rq.Nil(cache.Get(ctx, "key2"), "second value should have been evicted")
+}
