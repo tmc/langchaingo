fc33a0d7599a89c84114f915a2d0c90c07d390a7
Eli Bendersky <eliben@gmail.com>
2024-01-29 07:42:10 -0800

    move all tests to shared_test

--- llms/googleai/shared_test/shared_test.go
+++ llms/googleai/shared_test/shared_test.go
@@ -1,3 +1,4 @@
+// nolint
 package shared_test
 
 import (
@@ -11,6 +12,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"github.com/tmc/langchaingo/embeddings"
 	"github.com/tmc/langchaingo/llms"
 	"github.com/tmc/langchaingo/llms/googleai"
 	"github.com/tmc/langchaingo/llms/googleai/vertex"
@@ -59,6 +61,33 @@ func funcName(f any) string {
 	return parts[len(parts)-1]
 }
 
+type testFunc func(*testing.T, llms.Model)
+
+// testFuncs is a list of all test functions in this file to run with both
+// client types.
+var testFuncs = []testFunc{
+	testMultiContentText,
+	testGenerateFromSinglePrompt,
+	testMultiContentTextChatSequence,
+	testMultiContentImage,
+	testEmbeddings,
+	testCandidateCountSetting,
+	testMaxTokensSetting,
+}
+
+func TestShared(t *testing.T) {
+	for _, f := range testFuncs {
+		t.Run(fmt.Sprintf("%s-googleai", funcName(f)), func(t *testing.T) {
+			llm := newGoogleAIClient(t)
+			f(t, llm)
+		})
+		t.Run(fmt.Sprintf("%s-vertex", funcName(f)), func(t *testing.T) {
+			llm := newVertexClient(t)
+			f(t, llm)
+		})
+	}
+}
+
 func testMultiContentText(t *testing.T, llm llms.Model) {
 	t.Helper()
 	t.Parallel()
@@ -82,21 +111,142 @@ func testMultiContentText(t *testing.T, llm llms.Model) {
 	assert.Regexp(t, "(?i)dog|canid|canine", c1.Content)
 }
 
-type testFunc func(*testing.T, llms.Model)
+func testGenerateFromSinglePrompt(t *testing.T, llm llms.Model) {
+	t.Helper()
+	t.Parallel()
 
-var testFuncs = []testFunc{
-	testMultiContentText,
+	prompt := "name all the planets in the solar system"
+	rsp, err := llms.GenerateFromSinglePrompt(context.Background(), llm, prompt)
+	require.NoError(t, err)
+
+	assert.Regexp(t, "(?i)jupiter", rsp)
 }
 
-func TestShared(t *testing.T) {
-	for _, f := range testFuncs {
-		t.Run(fmt.Sprintf("%s-googleai", funcName(f)), func(t *testing.T) {
-			llm := newGoogleAIClient(t)
-			f(t, llm)
-		})
-		t.Run(fmt.Sprintf("%s-vertex", funcName(f)), func(t *testing.T) {
-			llm := newVertexClient(t)
-			f(t, llm)
-		})
+func testMultiContentTextChatSequence(t *testing.T, llm llms.Model) {
+	t.Helper()
+	t.Parallel()
+
+	content := []llms.MessageContent{
+		{
+			Role:  schema.ChatMessageTypeHuman,
+			Parts: []llms.ContentPart{llms.TextPart("Name some countries")},
+		},
+		{
+			Role:  schema.ChatMessageTypeAI,
+			Parts: []llms.ContentPart{llms.TextPart("Spain and Lesotho")},
+		},
+		{
+			Role:  schema.ChatMessageTypeHuman,
+			Parts: []llms.ContentPart{llms.TextPart("Which if these is larger?")},
+		},
+	}
+
+	rsp, err := llm.GenerateContent(context.Background(), content, llms.WithModel("gemini-pro"))
+	require.NoError(t, err)
+
+	assert.NotEmpty(t, rsp.Choices)
+	c1 := rsp.Choices[0]
+	assert.Regexp(t, "(?i)spain.*larger", c1.Content)
+}
+
+func testMultiContentImage(t *testing.T, llm llms.Model) {
+	t.Helper()
+	t.Parallel()
+
+	parts := []llms.ContentPart{
+		llms.ImageURLPart("https://github.com/tmc/langchaingo/blob/main/docs/static/img/parrot-icon.png?raw=true"),
+		llms.TextPart("describe this image in detail"),
+	}
+	content := []llms.MessageContent{
+		{
+			Role:  schema.ChatMessageTypeHuman,
+			Parts: parts,
+		},
+	}
+
+	rsp, err := llm.GenerateContent(context.Background(), content, llms.WithModel("gemini-pro-vision"))
+	require.NoError(t, err)
+
+	assert.NotEmpty(t, rsp.Choices)
+	c1 := rsp.Choices[0]
+	assert.Regexp(t, "(?i)parrot", c1.Content)
+}
+
+func testEmbeddings(t *testing.T, llm llms.Model) {
+	t.Helper()
+	t.Parallel()
+
+	texts := []string{"foo", "parrot"}
+	emb := llm.(embeddings.EmbedderClient)
+	res, err := emb.CreateEmbedding(context.Background(), texts)
+	require.NoError(t, err)
+
+	assert.Equal(t, len(texts), len(res))
+	assert.NotEmpty(t, res[0])
+	assert.NotEmpty(t, res[1])
+}
+
+func testCandidateCountSetting(t *testing.T, llm llms.Model) {
+	t.Helper()
+
+	parts := []llms.ContentPart{
+		llms.TextPart("Name five countries in Africa"),
+	}
+	content := []llms.MessageContent{
+		{
+			Role:  schema.ChatMessageTypeHuman,
+			Parts: parts,
+		},
+	}
+
+	{
+		rsp, err := llm.GenerateContent(context.Background(), content,
+			llms.WithCandidateCount(1), llms.WithTemperature(1))
+		require.NoError(t, err)
+
+		assert.Len(t, rsp.Choices, 1)
+	}
+
+	// TODO: test multiple candidates when the backend supports it
+}
+
+func testMaxTokensSetting(t *testing.T, llm llms.Model) {
+	t.Helper()
+	t.Parallel()
+
+	parts := []llms.ContentPart{
+		llms.TextPart("I'm a pomeranian"),
+		llms.TextPart("Describe my taxonomy, health and care"),
+	}
+	content := []llms.MessageContent{
+		{
+			Role:  schema.ChatMessageTypeHuman,
+			Parts: parts,
+		},
+	}
+
+	// First, try this with a very low MaxTokens setting for such a query; expect
+	// a stop reason that max of tokens was reached.
+	{
+		rsp, err := llm.GenerateContent(context.Background(), content,
+			llms.WithMaxTokens(16))
+		require.NoError(t, err)
+
+		assert.NotEmpty(t, rsp.Choices)
+		c1 := rsp.Choices[0]
+		assert.Regexp(t, "(?i)MaxTokens", c1.StopReason)
+	}
+
+	// Now, try it again with a much larger MaxTokens setting and expect to
+	// finish successfully and generate a response.
+	{
+		rsp, err := llm.GenerateContent(context.Background(), content,
+			llms.WithMaxTokens(2048))
+		require.NoError(t, err)
+
+		assert.NotEmpty(t, rsp.Choices)
+		c1 := rsp.Choices[0]
+		assert.Regexp(t, "(?i)stop", c1.StopReason)
+		assert.Regexp(t, "(?i)dog|canid|canine", c1.Content)
 	}
 }
