58f56553f4c0f9968b3251a1f287fe97c9e7ae62
filepang <filepang@gmail.com>
2024-02-20 18:36:27 +0900

    examples: Add inmemory vectorstore example

---
+++ examples/inmemory-vectorstore-example/README.md
@@ -0,0 +1,9 @@
+# inmemory vector store with OpenAI embeddings
+
+Run the example:
+
+```shell
+export OPENAI_API_KEY=<your key>
+go run inmemory_vectorstore_example.go
+```
+
---
+++ examples/inmemory-vectorstore-example/inmemory_vectorstore_example.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/tmc/langchaingo/llms/openai"
+	"github.com/tmc/langchaingo/schema"
+	"github.com/tmc/langchaingo/vectorstores/inmemory"
+
+	"github.com/tmc/langchaingo/embeddings"
+	"github.com/tmc/langchaingo/vectorstores"
+)
+
+func main() {
+	// Create an embeddings client using the OpenAI API. Requires environment variable OPENAI_API_KEY to be set.
+	llm, err := openai.New()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	e, err := embeddings.NewEmbedder(llm)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Create a new inmemory store.
+	ctx := context.Background()
+	store, err := inmemory.New(
+		ctx,
+		inmemory.WithEmbedder(e),
+		inmemory.WithVectorSize(1536),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Add documents to the inmemory store.
+	_, err = store.AddDocuments(context.Background(), []schema.Document{
+		{
+			PageContent: "Tokyo",
+			Metadata: map[string]any{
+				"population": 38,
+				"area":       2190,
+			},
+		},
+		{
+			PageContent: "Paris",
+			Metadata: map[string]any{
+				"population": 11,
+				"area":       105,
+			},
+		},
+		{
+			PageContent: "London",
+			Metadata: map[string]any{
+				"population": 9.5,
+				"area":       1572,
+			},
+		},
+		{
+			PageContent: "Santiago",
+			Metadata: map[string]any{
+				"population": 6.9,
+				"area":       641,
+			},
+		},
+		{
+			PageContent: "Buenos Aires",
+			Metadata: map[string]any{
+				"population": 15.5,
+				"area":       203,
+			},
+		},
+		{
+			PageContent: "Rio de Janeiro",
+			Metadata: map[string]any{
+				"population": 13.7,
+				"area":       1200,
+			},
+		},
+		{
+			PageContent: "Sao Paulo",
+			Metadata: map[string]any{
+				"population": 22.6,
+				"area":       1523,
+			},
+		},
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Search for similar documents.
+	docs, err := store.SimilaritySearch(ctx, "japan", 1)
+	fmt.Println(docs) // Tokyo
+
+	// Search for similar documents using score threshold.
+	docs, err = store.SimilaritySearch(ctx, "only cities in south america", 10, vectorstores.WithScoreThreshold(0.80))
+	fmt.Println(docs) // Buenos Aires, Sao Paulo, Rio de Janeiro
+
+	// Search for similar documents using score threshold and metadata filter.
+	// Metadata filter for pgvector only supports key-value pairs for now.
+	filter := map[string]any{"area": 1523} // Sao Paulo
+
+	docs, err = store.SimilaritySearch(ctx, "only cities in south america",
+		10,
+		vectorstores.WithScoreThreshold(0.80),
+		vectorstores.WithFilters(filter),
+	)
+	fmt.Println(docs) // Sao Paulo
+}
