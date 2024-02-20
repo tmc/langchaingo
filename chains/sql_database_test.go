4b7129c31a1f3f40d91dc0df7f598fbb74e487fb
devin.yf <dyfsquall@qq.com>
2024-02-20 12:06:33 +0800

    update test: all case inline

--- chains/sql_database_test.go
+++ chains/sql_database_test.go
@@ -2,7 +2,6 @@ package chains
 
 import (
 	"context"
-	"fmt"
 	"os"
 	"testing"
 
@@ -47,15 +46,13 @@ func TestSQLDatabaseChain_Call(t *testing.T) {
 	t.Log(ret)
 }
 
-type extractMultiLineSQLTestCase struct {
-	inputStr string
-	expected string
-}
-
 func TestExtractSQLQuery(t *testing.T) {
 	t.Parallel()
 
-	cases := []extractMultiLineSQLTestCase{
+	cases := []struct {
+		inputStr string
+		expected string
+	}{
 		{
 			inputStr: "SELECT * FROM example_table;",
 			expected: "SELECT * FROM example_table;",
@@ -78,42 +75,49 @@ func TestExtractSQLQuery(t *testing.T) {
 			expected: "SELECT * FROM example_table;",
 		},
 		{
-			inputStr: "```sql\nSELECT * FROM example_table;\n```\nSQLResult: 3 (this is not a real data)\nAnswer: There are 3 data in the table. (this is not a real data)\n",
+			inputStr: "```sql" + `
+			SELECT * FROM example_table;
+			` + "```" + `
+			SQLResult: 3 (this is not a real data)
+			Answer: There are 3 data in the table. (this is not a real data)`,
 			expected: "SELECT * FROM example_table;",
 		},
-	}
+		{ // multi-line sql query with markdown symbols and redundant text above and below
+			inputStr: `
+			I am also a clumsy llm model, I don't fully understand the prompt
+			And accidentally put some extra text here.
 
-	// similar to the above test cases, but with multi line sql query.
-	multiLineCases := buildMultiLineSQLTestCases()
-	cases = append(cases, multiLineCases...)
-
-	for _, tc := range cases {
-		filterQuerySyntax := extractSQLQuery(tc.inputStr)
-		require.Equal(t, tc.expected, filterQuerySyntax)
-	}
-}
-
-func buildMultiLineSQLTestCases() []extractMultiLineSQLTestCase {
-	cases := []extractMultiLineSQLTestCase{}
-
-	// mock multi line sql query.
-	bareMultiLineSQLQuery := `
-		SELECT
-			orders.order_id,
-			customers.customer_name,
-			orders.order_date
-		FROM
-			orders
-		INNER JOIN customers ON orders.customer_id = customers.customer_id
-		WHERE
-			orders.order_date >= '2023-01-01'
-		ORDER BY
-			orders.order_date;
-		`
-
-	// the same multi line sql query above with no indentation,
-	// this is the expected result after the extract function.
-	expectedQuery := `SELECT
+			SQLQuery: 
+			` + "```sql\n" + `
+			SELECT
+				order_id,
+				customer_name,
+				order_date
+			FROM orders;
+			` + "```" + `
+			SQLResult: xxx (this is not a real data)
+			Answer: some illusion answer. (this is not a real data)`,
+			expected: `SELECT
+order_id,
+customer_name,
+order_date
+FROM orders;`,
+		},
+		{ // slightly complexed multi-line query, no extra text before but only with redundant text after
+			inputStr: `SELECT
+				orders.order_id,
+				customers.customer_name,
+				orders.order_date
+			FROM
+				orders
+			INNER JOIN customers ON orders.customer_id = customers.customer_id
+			WHERE
+				orders.order_date >= '2023-01-01'
+			ORDER BY
+				orders.order_date;
+			SQLResult: xxx (this is not a real data)
+			Answer: some illusion answer. (this is not a real data)`,
+			expected: `SELECT
 orders.order_id,
 customers.customer_name,
 orders.order_date
@@ -123,54 +127,12 @@ INNER JOIN customers ON orders.customer_id = customers.customer_id
 WHERE
 orders.order_date >= '2023-01-01'
 ORDER BY
-orders.order_date;`
-
-	multiLineSQLWithPollutedStrings := polluteSQLSyntaxTestCase(bareMultiLineSQLQuery)
-
-	for _, pollutedStr := range multiLineSQLWithPollutedStrings {
-		cases = append(cases, extractMultiLineSQLTestCase{pollutedStr, expectedQuery})
+orders.order_date;`,
+		},
 	}
 
-	return cases
-}
-
-// different llm model result text format differently.
-// this function is used to pollute the sql query syntax text to simulate the different format.
-func polluteSQLSyntaxTestCase(sql string) []string {
-	// simulate the return output formot-1: contain illusion text above and below
-	// some extra text here.
-	// SQLQuery SELECT xxx... ;
-	// SQLResult: 3 (illusion data)
-	// Answer: There are 3 data in the table. (illusion data)
-	taintedOutput1 := `
-I am a clumsy llm model
-I just feel good to put some extra text here.
-SQLQuery: %s
-SQLResult: 3 (illusion data)
-Answer: There are 3 data in the table. (illusion data)
-`
-
-	// simulate the return output formot-2: contain illusion text below
-	// SELECT xxx... ;
-	// SQLResult: 3 (illusion data)
-	// Answer: There are 3 data in the table. (illusion data)
-	taintedOutput2 := `
-%s
-SQLResult: 3 (illusion data)
-Answer: There are 3 data in the table. (illusion data)
-`
-
-	// simulate the return output formot-3: contain markdown symbols
-	// ```sql
-	// SELECT xxx... ;
-	// ```
-	// SQLResult: 3 (illusion data)
-	// Answer: There are 3 data in the table. (illusion data)
-	taintedOutput3 := "```sql\n%s\n```\nSQLResult: 3 (illusion data)\nAnswer: There are 3 data in the table. (illusion data)\n"
-
-	polluteSQL1 := fmt.Sprintf(taintedOutput1, sql)
-	polluteSQL2 := fmt.Sprintf(taintedOutput2, sql)
-	polluteSQL3 := fmt.Sprintf(taintedOutput3, sql)
-
-	return []string{polluteSQL1, polluteSQL2, polluteSQL3}
+	for _, tc := range cases {
+		filterQuerySyntax := extractSQLQuery(tc.inputStr)
+		require.Equal(t, tc.expected, filterQuerySyntax)
+	}
 }
