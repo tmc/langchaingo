9aa84dda2b58ed2502fb374079f90b8ab4063aac
Eli Bendersky <eliben@gmail.com>
2024-02-22 17:48:57 -0800

    chains: add test with GoogleAI (#628)
    Resurrect test from #625, but add explicit settings until #626 is resolved

--- chains/llm_test.go
+++ chains/llm_test.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/stretchr/testify/require"
 	"github.com/tmc/langchaingo/callbacks"
+	"github.com/tmc/langchaingo/llms/googleai"
 	"github.com/tmc/langchaingo/llms/openai"
 	"github.com/tmc/langchaingo/prompts"
 )
@@ -54,4 +55,41 @@ func TestLLMChainWithChatPromptTemplate(t *testing.T) {
 	})
 	require.NoError(t, err)
 	require.Equal(t, "AI: foo\nHuman: boo", result)
+}
+
+func TestLLMChainWithGoogleAI(t *testing.T) {
+	t.Parallel()
+	genaiKey := os.Getenv("GENAI_API_KEY")
+	if genaiKey == "" {
+		t.Skip("GENAI_API_KEY not set")
+	}
+	model, err := googleai.New(context.Background(), googleai.WithAPIKey(genaiKey))
+	require.NoError(t, err)
+	require.NoError(t, err)
+	model.CallbacksHandler = callbacks.LogHandler{}
+
+	prompt := prompts.NewPromptTemplate(
+		"What is the capital of {{.country}}",
+		[]string{"country"},
+	)
+	require.NoError(t, err)
+
+	chain := NewLLMChain(model, prompt)
+
+	// chains tramples over defaults for options, so setting these options
+	// explicitly is required until https://github.com/tmc/langchaingo/issues/626
+	// is fully resolved.
+	result, err := Predict(context.Background(), chain,
+		map[string]any{
+			"country": "France",
+		},
+		WithCallback(callbacks.LogHandler{}),
+		WithModel("gemini-pro"),
+		WithMaxTokens(200),
+		WithStopWords(nil),
+		WithTopK(3),
+		WithTopP(0.95),
+	)
+	require.NoError(t, err)
+	require.True(t, strings.Contains(result, "Paris"))
 }
