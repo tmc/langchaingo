a4fe08eaaefa77ae22c6c37656c405fefef149ef
Alexandre E Souza <alexandre@dev2learn.com>
2024-03-16 01:01:43 -0300

    fix: test done

--- llms/llamafile/llamafilellm_test.go
+++ llms/llamafile/llamafilellm_test.go
@@ -8,7 +8,6 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"github.com/tmc/langchaingo/embeddings"
 	"github.com/tmc/langchaingo/llms"
 	"github.com/tmc/langchaingo/schema"
 )
@@ -84,44 +83,4 @@ func TestCreateEmbedding(t *testing.T) {
 	embeddings, err := llm.CreateEmbedding(context.Background(), []string{"hello", "world"})
 	require.NoError(t, err)
 	assert.Len(t, embeddings, 2)
-}
-
-// teste example complet
-func TestLLM(t *testing.T) {
-	t.Parallel()
-
-	llm, err := New()
-
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	parts := []llms.ContentPart{
-		llms.TextContent{Text: "Brazil is a country? answer yes or no"},
-	}
-	content := []llms.MessageContent{
-		{
-			Role:  schema.ChatMessageTypeHuman,
-			Parts: parts,
-		},
-	}
-
-	_, err = embeddings.NewEmbedder(llm)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	var sb strings.Builder
-	rsp, err := llm.GenerateContent(context.Background(), content,
-		llms.WithStreamingFunc(func(ctx context.Context, chunk []byte) error {
-			sb.Write(chunk)
-			return nil
-		}))
-
-	require.NoError(t, err)
-
-	assert.NotEmpty(t, rsp.Choices)
-	c1 := rsp.Choices[0]
-	assert.Regexp(t, "yes", strings.ToLower(c1.Content))
-	assert.Regexp(t, "yes", strings.ToLower(sb.String()))
 }
