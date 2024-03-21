5069ea2804b7caa7c79c522b31daf747505f2d64
Travis Cline <travis.cline@gmail.com>
2024-03-20 17:18:47 -0700

    examples: Use new debugging helper in example

--- examples/openai-completion-example-with-http-debugging/go.mod
+++ examples/openai-completion-example-with-http-debugging/go.mod
@@ -4,7 +4,7 @@ go 1.21
 
 toolchain go1.21.4
 
-require github.com/tmc/langchaingo v0.1.7
+require github.com/tmc/langchaingo v0.1.8-0.20240321002625-491288f2a685
 
 require (
 	github.com/dlclark/regexp2 v1.10.0 // indirect
--- examples/openai-completion-example-with-http-debugging/openai_completion_example.go
+++ examples/openai-completion-example-with-http-debugging/openai_completion_example.go
@@ -5,7 +5,6 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"net/http"
 	"net/http/httputil"
 
 	"github.com/tmc/langchaingo/llms"
@@ -19,11 +18,7 @@ func main() {
 	flag.Parse()
 	var opts []openai.Option
 	if *flagDebugHTTP {
-		opts = append(opts, openai.WithHTTPClient(&http.Client{
-			Transport: &logTransport{
-				Transport: http.DefaultTransport, // Use http.DefaultTransport as the underlying transport
-			},
-		}))
+		opts = append(opts, openai.WithHTTPClient(httputil.DebugHTTPClient))
 	}
 
 	llm, err := openai.New(opts...)
@@ -41,34 +36,4 @@ func main() {
 	}
 
 	fmt.Println(completion)
-}
-
-// logTransport wraps around an existing http.RoundTripper, allowing us to
-// intercept and log the request and response.
-type logTransport struct {
-	Transport http.RoundTripper
-}
-
-// RoundTrip executes a single HTTP transaction and logs the request and response.
-func (c *logTransport) RoundTrip(req *http.Request) (*http.Response, error) {
-	// Log the request
-	requestDump, err := httputil.DumpRequestOut(req, true)
-	if err == nil {
-		log.Println("Request:\n" + string(requestDump))
-	} else {
-		log.Println("Error dumping request:", err)
-	}
-	// Use the underlying Transport to execute the request
-	resp, err := c.Transport.RoundTrip(req)
-	if err != nil {
-		return nil, err // Return early if there's an error
-	}
-	// Log the response
-	responseDump, err := httputil.DumpResponse(resp, true)
-	if err == nil {
-		log.Println("Response:\n" + string(responseDump))
-	} else {
-		log.Println("Error dumping response:", err)
-	}
-	return resp, err
 }
