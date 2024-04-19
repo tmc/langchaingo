d8dcffdb0cfc86e5bf9b3cee9630d6ac5353d39f
Joey Chilson <joeychilson@outlook.com>
2024-04-19 01:15:41 -0500

    embeddings: add voyageai embedder

---
+++ embeddings/voyageai/options.go
@@ -0,0 +1,76 @@
+package voyageai
+
+import (
+	"errors"
+	"net/http"
+	"os"
+)
+
+const (
+	_defaultBaseURL       = "https://api.voyageai.com/v1"
+	_defaultBatchSize     = 512
+	_defaultStripNewLines = true
+	_defaultModel         = "voyage-2"
+)
+
+// Option is a function type that can be used to modify the client.
+type Option func(v *VoyageAI)
+
+// WithModel is an option for providing the model name to use.
+func WithModel(model string) Option {
+	return func(v *VoyageAI) {
+		v.Model = model
+	}
+}
+
+// WithClient is an option for providing a custom http client.
+func WithClient(client http.Client) Option {
+	return func(v *VoyageAI) {
+		v.client = &client
+	}
+}
+
+// WithToken is an option for providing the VoyageAI token.
+func WithToken(token string) Option {
+	return func(v *VoyageAI) {
+		v.token = token
+	}
+}
+
+// WithStripNewLines is an option for specifying the should it strip new lines.
+func WithStripNewLines(stripNewLines bool) Option {
+	return func(v *VoyageAI) {
+		v.StripNewLines = stripNewLines
+	}
+}
+
+// WithBatchSize is an option for specifying the batch size.
+func WithBatchSize(batchSize int) Option {
+	return func(v *VoyageAI) {
+		v.BatchSize = batchSize
+	}
+}
+
+func applyOptions(opts ...Option) (*VoyageAI, error) {
+	o := &VoyageAI{
+		baseURL:       _defaultBaseURL,
+		Model:         _defaultModel,
+		StripNewLines: _defaultStripNewLines,
+		BatchSize:     _defaultBatchSize,
+	}
+	for _, opt := range opts {
+		opt(o)
+	}
+	if o.client == nil {
+		o.client = http.DefaultClient
+	}
+	if o.token == "" {
+		token := os.Getenv("VOYAGEAI_API_KEY")
+		if token != "" {
+			o.token = token
+		} else {
+			return nil, errors.New("missing the VoyageAI API key, set it as VOYAGEAI_API_KEY environment variable")
+		}
+	}
+	return o, nil
+}
---
+++ embeddings/voyageai/voyageai.go
@@ -0,0 +1,137 @@
+package voyageai
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/tmc/langchaingo/embeddings"
+)
+
+var _ embeddings.Embedder = &VoyageAI{}
+
+// VoyageAI is the embedder using the VoyageAI api.
+type VoyageAI struct {
+	baseURL       string
+	token         string
+	client        *http.Client
+	Model         string
+	StripNewLines bool
+	BatchSize     int
+}
+
+// NewVoyageAI returns a new embedding client that uses the VoyageAI api.
+// The default model is "voyage-2". Use `WithModel` to change the model.
+func NewVoyageAI(opts ...Option) (*VoyageAI, error) {
+	v, err := applyOptions(opts...)
+	if err != nil {
+		return nil, err
+	}
+	return v, nil
+}
+
+type embeddingResponse struct {
+	Data []struct {
+		Embedding []float32 `json:"embedding"`
+	}
+}
+
+type embedDocumentsRequest struct {
+	Model     string   `json:"model"`
+	Input     []string `json:"input"`
+	InputType string   `json:"input_type"`
+}
+
+func (v *VoyageAI) EmbedDocuments(ctx context.Context, texts []string) ([][]float32, error) {
+	batchedTexts := embeddings.BatchTexts(
+		embeddings.MaybeRemoveNewLines(texts, v.StripNewLines),
+		v.BatchSize,
+	)
+
+	embeddings := make([][]float32, 0, len(texts))
+	for _, batch := range batchedTexts {
+		req := embedDocumentsRequest{
+			Model:     v.Model,
+			Input:     batch,
+			InputType: "document",
+		}
+
+		resp, err := v.request(ctx, "/embeddings", req)
+		if err != nil {
+			return nil, fmt.Errorf("embed documents request error: %w", err)
+		}
+		defer resp.Body.Close()
+
+		if resp.StatusCode != http.StatusOK {
+			return nil, v.decodeError(resp)
+		}
+
+		var embeddingResp embeddingResponse
+		if err := json.NewDecoder(resp.Body).Decode(&embeddingResp); err != nil {
+			return nil, err
+		}
+
+		for _, data := range embeddingResp.Data {
+			embeddings = append(embeddings, data.Embedding)
+		}
+	}
+	return embeddings, nil
+}
+
+type embedQueryRequest struct {
+	Model     string `json:"model"`
+	Input     string `json:"input"`
+	InputType string `json:"input_type"`
+}
+
+func (v *VoyageAI) EmbedQuery(ctx context.Context, text string) ([]float32, error) {
+	req := embedQueryRequest{
+		Model:     v.Model,
+		Input:     text,
+		InputType: "query",
+	}
+	resp, err := v.request(ctx, "/embeddings", req)
+	if err != nil {
+		return nil, fmt.Errorf("embed query request error: %w", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, v.decodeError(resp)
+	}
+
+	var embeddingResp embeddingResponse
+	if err := json.NewDecoder(resp.Body).Decode(&embeddingResp); err != nil {
+		return nil, err
+	}
+	return embeddingResp.Data[0].Embedding, nil
+}
+
+func (v *VoyageAI) request(ctx context.Context, path string, body any) (*http.Response, error) {
+	reqBody, err := json.Marshal(body)
+	if err != nil {
+		return nil, err
+	}
+
+	httpReq, err := http.NewRequestWithContext(ctx, http.MethodPost, v.baseURL+path, bytes.NewBuffer(reqBody))
+	if err != nil {
+		return nil, err
+	}
+
+	httpReq.Header.Set("Authorization", "Bearer "+v.token)
+	httpReq.Header.Set("Content-Type", "application/json")
+
+	return v.client.Do(httpReq)
+}
+
+func (v *VoyageAI) decodeError(resp *http.Response) error {
+	var errResp struct {
+		Detail string `json:"detail"`
+	}
+	if err := json.NewDecoder(resp.Body).Decode(&errResp); err != nil {
+		return fmt.Errorf("unexpected error: %w", err)
+	}
+	return fmt.Errorf("embedding error: %s", errResp.Detail)
+}
