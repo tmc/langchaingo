a700020f424aba36900bb6ccd97f4fbfbc346693
Simon Klinkert <simon.klinkert@gmail.com>
2024-04-22 17:10:13 +0200

    Add groq completion example

---
+++ examples/groq-completion-example/groq_completion_example.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/tmc/langchaingo/llms"
+	"github.com/tmc/langchaingo/llms/openai"
+)
+
+func main() {
+	llm, err := openai.New(
+		openai.WithModel("llama3-8b-8192"),
+		openai.WithBaseURL("https://api.groq.com/openai/v1"),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx := context.Background()
+	completion, err := llms.GenerateFromSinglePrompt(ctx,
+		llm,
+		"The first man to walk on the moon",
+		llms.WithTemperature(0.8),
+		llms.WithStopWords([]string{"Armstrong"}),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(completion)
+}
