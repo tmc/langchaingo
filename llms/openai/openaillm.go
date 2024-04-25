c986d61fcfdce723543855e248010cea68124677
lowjiansheng <15527690+lowjiansheng@users.noreply.github.com>
2024-04-25 16:08:18 +0800

    add kwargs parameter

--- llms/openai/openaillm.go
+++ llms/openai/openaillm.go
@@ -109,6 +109,7 @@ func (o *LLM) GenerateContent(ctx context.Context, messages []llms.MessageConten
 
 		FunctionCallBehavior: openaiclient.FunctionCallBehavior(opts.FunctionCallBehavior),
 		Seed:                 opts.Seed,
+		Kwargs:               opts.Kwargs,
 	}
 	if opts.JSONMode {
 		req.ResponseFormat = ResponseFormatJSON
