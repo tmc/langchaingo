b4fa43ca66842903ac6cbf7256fae3a16a08faaa
Ankit Arora <ankit.j.arora@gmail.com>
2024-05-27 01:29:45 +0530

    feature(OpenAI function agent): support for handling multiple tool calls for OpenAI function agent.

--- agents/executor_test.go
+++ agents/executor_test.go
@@ -2,19 +2,18 @@ package agents_test
 
 import (
 	"context"
-	"github.com/tmc/langchaingo/llms"
-	"os"
-	"strings"
-	"testing"
-
 	"github.com/stretchr/testify/require"
 	"github.com/tmc/langchaingo/agents"
 	"github.com/tmc/langchaingo/chains"
+	"github.com/tmc/langchaingo/llms"
 	"github.com/tmc/langchaingo/llms/openai"
 	"github.com/tmc/langchaingo/prompts"
 	"github.com/tmc/langchaingo/schema"
 	"github.com/tmc/langchaingo/tools"
 	"github.com/tmc/langchaingo/tools/serpapi"
+	"os"
+	"strings"
+	"testing"
 )
 
 type testAgent struct {
