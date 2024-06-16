e380482d40dddaf680466adf6d6d1eda74abb5ba
Ashad <93534298+Ashad001@users.noreply.github.com>
2024-06-17 01:44:54 +0500

    examples: Correct API key usage for Groq integration (#893)
    Correct API key usage for Groq integration

--- examples/groq-completion-example/groq_completion_example.go
+++ examples/groq-completion-example/groq_completion_example.go
@@ -4,15 +4,25 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/tmc/langchaingo/llms"
 	"github.com/tmc/langchaingo/llms/openai"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	apiKey := os.Getenv("GROQ_API_KEY")
+
 	llm, err := openai.New(
 		openai.WithModel("llama3-8b-8192"),
 		openai.WithBaseURL("https://api.groq.com/openai/v1"),
+		openai.WithToken(apiKey),
 	)
 	if err != nil {
 		log.Fatal(err)
