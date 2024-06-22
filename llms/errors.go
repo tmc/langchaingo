2c2271cf89c992b56c79bda460ec9973ea45c6e9
William Cook <web@casadecook.com>
2024-06-22 12:15:54 -0400

    golangci-lint: fix missing periods in comments

--- llms/errors.go
+++ llms/errors.go
@@ -3,9 +3,9 @@ package llms
 import "errors"
 
 var (
-	// ErrEmptyResponse is thrown when an LLM returns an empty response
+	// ErrEmptyResponse is thrown when an LLM returns an empty response.
 	ErrEmptyResponse = errors.New("no response")
 	// ErrIncompleteEmbedding is thrown when the length of an embedding
-	// request does not match the length of the returned embeddings array
+	// request does not match the length of the returned embeddings array.
 	ErrIncompleteEmbedding = errors.New("not all input got embedded")
 )
