cc420948ff14d6be1b8da5efb643347d2691ad71
Travis Cline <travis.cline@gmail.com>
2024-09-12 15:59:56 -0700

    openai: add reasoning tokens to usage response (#1015)

--- llms/openai/openaillm.go
+++ llms/openai/openaillm.go
@@ -153,6 +153,7 @@ func (o *LLM) GenerateContent(ctx context.Context, messages []llms.MessageConten
 				"CompletionTokens": result.Usage.CompletionTokens,
 				"PromptTokens":     result.Usage.PromptTokens,
 				"TotalTokens":      result.Usage.TotalTokens,
+				"ReasoningTokens":  result.Usage.CompletionTokensDetails.ReasoningTokens,
 			},
 		}
 
