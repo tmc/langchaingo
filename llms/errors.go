498bbdb4e8fde1a639bc627527ec32f9ec5cd295
William Cook <web@casadecook.com>
2024-09-12 21:04:12 -0400

    golangci-lint: remove duplicate 'is' in docstring

--- llms/errors.go
+++ llms/errors.go
@@ -5,7 +5,7 @@ import "errors"
 var (
 	// ErrEmptyResponse is returned when an LLM returns an empty response.
 	ErrEmptyResponse = errors.New("no response")
-	// ErrIncompleteEmbedding is is returned when the length of an embedding
+	// ErrIncompleteEmbedding is returned when the length of an embedding
 	// request does not match the length of the returned embeddings array.
 	ErrIncompleteEmbedding = errors.New("not all input got embedded")
 )
