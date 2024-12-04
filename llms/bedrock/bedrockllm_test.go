3bcfe62d42664e88bf79a8a7214f14f2a3d28cf8
Oryan Moshe <43927816+oryanmoshe@users.noreply.github.com>
2024-12-04 16:42:14 +0200

    ✅test(bedrock): Add tests for nova models and multimodal image inputs

--- llms/bedrock/bedrockllm_test.go
+++ llms/bedrock/bedrockllm_test.go
@@ -66,27 +66,145 @@ func TestAmazonOutput(t *testing.T) {
 
 	// All the test models.
 	models := []string{
-		bedrock.ModelAi21J2MidV1,
-		bedrock.ModelAi21J2UltraV1,
-		bedrock.ModelAmazonTitanTextLiteV1,
-		bedrock.ModelAmazonTitanTextExpressV1,
-		bedrock.ModelAnthropicClaudeV3Sonnet,
-		bedrock.ModelAnthropicClaudeV3Haiku,
-		bedrock.ModelAnthropicClaudeV21,
-		bedrock.ModelAnthropicClaudeV2,
-		bedrock.ModelAnthropicClaudeInstantV1,
-		bedrock.ModelCohereCommandTextV14,
-		bedrock.ModelCohereCommandLightTextV14,
-		bedrock.ModelMetaLlama213bChatV1,
-		bedrock.ModelMetaLlama270bChatV1,
-		bedrock.ModelMetaLlama38bInstructV1,
-		bedrock.ModelMetaLlama370bInstructV1,
+		// bedrock.ModelAi21J2MidV1,
+		// bedrock.ModelAi21J2UltraV1,
+		// bedrock.ModelAmazonTitanTextLiteV1,
+		// bedrock.ModelAmazonTitanTextExpressV1,
+		// bedrock.ModelAnthropicClaudeV3Sonnet,
+		// bedrock.ModelAnthropicClaudeV3Haiku,
+		// bedrock.ModelAnthropicClaudeV21,
+		// bedrock.ModelAnthropicClaudeV2,
+		// bedrock.ModelAnthropicClaudeInstantV1,
+		// bedrock.ModelCohereCommandTextV14,
+		// bedrock.ModelCohereCommandLightTextV14,
+		// bedrock.ModelMetaLlama213bChatV1,
+		// bedrock.ModelMetaLlama270bChatV1,
+		// bedrock.ModelMetaLlama38bInstructV1,
+		// bedrock.ModelMetaLlama370bInstructV1,
+		bedrock.ModelAmazonNovaMicroV1,
+		bedrock.ModelAmazonNovaLiteV1,
+		bedrock.ModelAmazonNovaProV1,
 	}
 
 	for _, model := range models {
 		t.Logf("Model output for %s:-", model)
 
 		resp, err := llm.GenerateContent(ctx, msgs, llms.WithModel(model), llms.WithMaxTokens(512))
+		t.Logf("resp: %+v\n", resp)
+		if err != nil {
+			t.Fatal(err)
+		}
+		for i, choice := range resp.Choices {
+			t.Logf("Choice %d: %s", i, choice.Content)
+		}
+	}
+}
+
+func TestAmazonNova(t *testing.T) {
+	t.Parallel()
+
+	if os.Getenv("TEST_AWS") != "true" {
+		t.Skip("Skipping test, requires AWS access")
+	}
+
+	client, err := setUpTest()
+	if err != nil {
+		t.Fatal(err)
+	}
+	llm, err := bedrock.New(bedrock.WithClient(client))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	msgs := []llms.MessageContent{
+		{
+			Role: llms.ChatMessageTypeSystem,
+			Parts: []llms.ContentPart{
+				llms.TextPart("You know all about AI."),
+			},
+		},
+		{
+			Role: llms.ChatMessageTypeHuman,
+			Parts: []llms.ContentPart{
+				llms.TextPart("Explain AI in 10 words or less."),
+			},
+		},
+	}
+
+	// All the test models.
+	models := []string{
+		bedrock.ModelAmazonNovaMicroV1,
+		bedrock.ModelAmazonNovaLiteV1,
+		bedrock.ModelAmazonNovaProV1,
+	}
+
+	ctx := context.Background()
+
+	for _, model := range models {
+		llms.ShowMessageContents(os.Stdout, msgs)
+
+		resp, err := llm.GenerateContent(ctx, msgs, llms.WithModel(model), llms.WithMaxTokens(4096))
+		if err != nil {
+			t.Fatal(err)
+		}
+		for i, choice := range resp.Choices {
+			t.Logf("Choice %d: %s", i, choice.Content)
+		}
+	}
+}
+
+func TestAnthropicNovaImage(t *testing.T) {
+	t.Parallel()
+
+	if os.Getenv("TEST_AWS") != "true" {
+		t.Skip("Skipping test, requires AWS access")
+	}
+
+	client, err := setUpTest()
+	if err != nil {
+		t.Fatal(err)
+	}
+	llm, err := bedrock.New(bedrock.WithClient(client))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	image, err := os.ReadFile("wikipage.jpg")
+	mimeType := "image/jpeg"
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	msgs := []llms.MessageContent{
+		{
+			Role: llms.ChatMessageTypeSystem,
+			Parts: []llms.ContentPart{
+				llms.TextPart("You know all about AI."),
+			},
+		},
+		{
+			Role: llms.ChatMessageTypeHuman,
+			Parts: []llms.ContentPart{
+				llms.TextPart("Explain AI according to the image. Provide quotes from the image."),
+				llms.BinaryPart(mimeType, image),
+			},
+		},
+	}
+
+	// All the test models.
+	models := []string{
+		bedrock.ModelAmazonNovaLiteV1,
+		bedrock.ModelAmazonNovaProV1,
+		bedrock.ModelAnthropicClaudeV3Sonnet,
+	}
+
+	ctx := context.Background()
+
+	for _, model := range models {
+		llms.ShowMessageContents(os.Stdout, msgs)
+
+		resp, err := llm.GenerateContent(ctx, msgs, llms.WithModel(model), llms.WithMaxTokens(4096))
+		t.Logf("resp: %+v\n", resp)
 		if err != nil {
 			t.Fatal(err)
 		}
