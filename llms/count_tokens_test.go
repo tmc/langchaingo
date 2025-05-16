5bcf413f920816d6eb4e49cf4d05e1cfe165681e
Luis Camara <lcamara@vendasta.com>
2025-05-16 11:38:02 -0600

    chore: remove unnecessary variable

--- llms/count_tokens_test.go
+++ llms/count_tokens_test.go
@@ -54,7 +54,6 @@ func TestCountTokens(t *testing.T) {
 	}
 
 	for _, tt := range tests {
-		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
 			numTokens := CountTokens(tt.model, tt.text)
